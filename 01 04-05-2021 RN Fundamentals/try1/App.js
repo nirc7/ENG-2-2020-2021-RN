b151d83397e7e808b426f0e3d8f283393733eae5
nirc7 <i2cs2013@gmail.com>
2021-05-04 13:10:46 +0300

    rn2

--- 01 04-05-2021 RN Fundamentals/try1/App.js
+++ 01 04-05-2021 RN Fundamentals/try1/App.js
@@ -1,14 +1,52 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Nir's3 your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { showYes: false };
+  }
+  
+  
+  btnLogin = () => {
+    //alert(1);
+    console.log(2);
+    if (this.state.txt === '123') {
+      this.setState({ showYes: true });
+    }
+    else
+    {
+      this.setState({ showYes: false });
+    }
+  }
+
+  render() {
+    console.log(this.state != null ? this.state.txt : '');
+    return (
+      <View style={styles.container}>
+        <StatusBar style="auto" />
+        <Text style={styles.Txt}>Nir's your app!</Text>
+        <Text style={{
+          color: '#37f',
+          margin: 50
+        }}>hello world!</Text>
+
+
+        <TextInput
+          style={styles.input}
+          onChangeText={(text) => this.setState({ txt: text })}
+          placeholder="enter ur num..."
+          keyboardType="numeric"
+        />
+        <View style={{ margin: 20 }}>
+          <Button title="Login" onPress={this.btnLogin} />
+        </View>
+        {this.state.showYes && <Text style={{color:'green'}} >Yes</Text> }
+        { !this.state.showYes && <Text style={{color:'red'}} >No</Text> }
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
@@ -16,6 +54,17 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
+  },
+  Txt: {
+    color: 'red',
+    margin: 30,
+    fontSize: 30
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10
   },
 });
